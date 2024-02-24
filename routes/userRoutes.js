a0f0a7735dff9b093cd5248377ca72256d1b4185
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2024-02-24 17:46:57 +0200

    add sending email

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { userRegistrationSchema, userUpdateSubscriptionSchema } from "../schemas/userSchemas.js";
+import { userRegistrationSchema, userUpdateSubscriptionSchema, verificationEmailSchema } from "../schemas/userSchemas.js";
 import validateBody from "../helpers/validateBody.js";
 import {
   userSignup,
@@ -7,7 +7,9 @@ import {
   userLogout,
   currentUser,
   userUpdateSubscription,
-  addAvatar
+  addAvatar,
+  verificationEmail,
+  resendEmail
 } from "../controllers/usersControllers.js";
 import { verifyToken } from "../midellwares/checkToken.js";
 import upload from "../midellwares/upload.js";
@@ -18,7 +20,9 @@ userRouter.post("/register", validateBody(userRegistrationSchema), userSignup);
 userRouter.post("/login", validateBody(userRegistrationSchema), userSignIn);
 userRouter.post("/logout", verifyToken, userLogout);
 userRouter.get("/current", verifyToken, currentUser);
-userRouter.patch("/", verifyToken, validateBody(userUpdateSubscriptionSchema), userUpdateSubscription)
-userRouter.patch("/avatars", verifyToken, upload.single("avatar"), addAvatar )
+userRouter.patch("/", verifyToken, validateBody(userUpdateSubscriptionSchema), userUpdateSubscription);
+userRouter.patch("/avatars", verifyToken, upload.single("avatar"), addAvatar);
+userRouter.get("/verify/:verificationToken", verificationEmail);
+userRouter.post( "/verify", validateBody(verificationEmailSchema), resendEmail)
 
 export default userRouter;
